bab5007d0f1b52c8c594c8d87c54f6b1333b26e7
Jason Hoyt <hoytjmatthews@gmail.com>
2021-09-19 13:57:36 -0400

    save

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,14 +10,13 @@ export const projects = [
     id: 0,
   },
   {
-    title: "Next API",
-    description:
-      "Next.js App with links to News, Employee of the month and a Twitter link",
-    image: "/images/13.png",
-    tags: ["React", "WebRTC"],
-    source: "https://next-news-kappa.vercel.app/",
-    visit: "https://github.com/Hoyt1974/Next-News",
-    id: 2,
+    title: "Bootstrap",
+    description: "A simple website using bootstrap",
+    image: "/images/15.png",
+    tags: ["React", "JavaScript"],
+    source: "https://hackinghoyt.netlify.app/",
+    visit: "https://github.com/Hoyt1974/bootstrap",
+    id: 4,
   },
   {
     title: "Youtube Search",
@@ -29,15 +28,15 @@ export const projects = [
     visit: "https://github.com/Hoyt1974/Video-Search",
     id: 1,
   },
-
   {
-    title: "Bootstrap",
-    description: "A simple website using bootstrap",
-    image: "/images/bootstrap.png",
-    tags: ["React", "JavaScript"],
-    source: "https://hackinghoyt.netlify.app/",
-    visit: "https://hackinghoyt.netlify.app/",
-    id: 4,
+    title: "Next API",
+    description:
+      "Next.js App with links to News, Employee of the month and a Twitter link",
+    image: "/images/13.png",
+    tags: ["React", "Next.js"],
+    source: "https://next-news-kappa.vercel.app/",
+    visit: "https://github.com/Hoyt1974/Next-News",
+    id: 2,
   },
 ];
 
